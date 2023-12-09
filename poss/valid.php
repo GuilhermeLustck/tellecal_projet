17adbb50968b2c1d922370dd4d67654935b60774
GuilhermeLustck <150307505+GuilhermeLustck@users.noreply.github.com>
2023-12-09 17:46:15 -0300

    Atualizar o valid.php

--- poss/valid.php
+++ poss/valid.php
@@ -1,11 +1,13 @@
 <?php
     include 'verif.php';
+//muda o documento para json
     header('content-Type: application/json');
     
-    
+ //pega os dados do javascript
+
     $Email=$_POST['nome_login'];
     $Senha=$_POST['senha_login'];
     
-    
+ //chama a função para validar a entrada
     validar_entrada($Email,$Senha);
 ?>
