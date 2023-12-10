a1931ed7330cf9628741b2f0c9adc44e89849107
GuilhermeLustck <150307505+GuilhermeLustck@users.noreply.github.com>
2023-12-09 22:35:02 -0300

    Update perfil.php

--- perfil/perfil.php
+++ perfil/perfil.php
@@ -1,15 +1,16 @@
 <?php
+    //muda o o tipo do documento para json
     header('content-Type: application/json');
-
+    //inclue o arquivo da função no documento
     include '../poss/verif.php';
     
     session_start();
 
-    
+    //pega o id do usuario logado na sessão 
     $id=$_SESSION['indet'][0];
 
     if($id){
-
+        //busca o usuario no bando de dados
         perfil($id);
 
     }
