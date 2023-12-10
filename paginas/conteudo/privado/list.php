838eb56046cf65475c1de5cc587e21ce3bb83ec4
GuilhermeLustck <150307505+GuilhermeLustck@users.noreply.github.com>
2023-12-09 22:53:45 -0300

    Update list.php

--- paginas/conteudo/privado/list.php
+++ paginas/conteudo/privado/list.php
@@ -1,8 +1,9 @@
 <?php
+//arquivo responsavel pela chamada da função
 include '../../../poss/verif.php';
 header('content-Type: application/json');
 
-
+//função responsavel pela listagem de tudo do banco de dados
 lista_BD();
 
 
