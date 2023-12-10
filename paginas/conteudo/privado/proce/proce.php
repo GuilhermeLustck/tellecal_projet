f10cb72817367db94daadcaec06d3a9e069579cc
GuilhermeLustck <150307505+GuilhermeLustck@users.noreply.github.com>
2023-12-09 22:55:24 -0300

    Update proce.php

--- paginas/conteudo/privado/proce/proce.php
+++ paginas/conteudo/privado/proce/proce.php
@@ -1,7 +1,8 @@
 <?php
+    //arquivo responsavel pela saida do usuario master
     header('content-Type: application/json');
     include '../../../../poss/verif.php';
-
+    
     $res=$_POST["proce"];
 
     if($res=="sair"){
