1a98102a64f665f29da3c91bd7958b5a2872cd0c
GuilhermeLustck <150307505+GuilhermeLustck@users.noreply.github.com>
2023-12-09 22:08:29 -0300

    Update m_senha.php

--- perfil/m_senha/m_senha.php
+++ perfil/m_senha/m_senha.php
@@ -1,21 +1,23 @@
 <?php
+//muda do tipo de documento para json
 header('content-Type: application/json');
 include '../../poss/verif.php';
+
 $opsao=$_GET['opsao0'];
 
 
 
 
-
+//verifica o que vai ser mudado
 if($opsao=="Senha"){
 
     $valor=$_GET['senha0'];
     $id=$_GET['ID'];
 
-    if($opsao=="Senha"){
+    
 
-        update($opsao,$valor,$id);
-    }
+    update($opsao,$valor,$id);
+    
 }
 
 
